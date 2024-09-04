b46e1b8893dce7a21b2b4e8ffd7ec37208c89df2
R Sai Ashwin <sai.ashwin2409@gmail.com>
2024-09-04 15:07:17 +0530

    Helper script

--- ghidra_scripts/getMaskedCodeBytes.py
+++ ghidra_scripts/getMaskedCodeBytes.py
@@ -19,29 +19,10 @@ from ghidra.program.model.address import Address
 from ghidra.program.flatapi import FlatProgramAPI
 from ghidra.program.model.lang import OperandType
 
-# Set FIRMAL_DIR in env for output file location
-if 'FIRMAL_DIR' not in os.environ:
-	print("ERROR: env FIRMAL_DIR not set. Set FIRMAL_DIR for output directory")
-	exit(-1)
-
-op_path = os.environ['FIRMAL_DIR']
-if op_path[-1] != "/":
-	op_path += "/"
-
-timestr = time.strftime("%Y%m%d:%H%M%S")
-
-hashdb = sqlite3.connect(op_path + "hash.db")
-cur = hashdb.cursor()
-res = cur.execute("SELECT name FROM sqlite_master WHERE name='hashdump'")
-if(res.fetchone() is None):
-    cur.execute("CREATE TABLE hashdump(size, progname, progpath, funcname, vaddr, fileoff, hash)")
-
 cp = currentProgram()
 ctx = {}
 ctx["ARCH"] = cp.getLanguage().toString().split("/")[0]
 ctx["BITS"] = cp.getLanguage().toString().split("/")[2]
-ctx["DBPATH"] = op_path+"hash.db"
-ctx["TIME"] = timestr
 ctx["FILEPATH"] = cp.getExecutablePath()
 
 # Let script be run on headless mode with -noanalyse flag
@@ -106,7 +87,7 @@ def process_instruction(instruction):
             print("Unknown Architecture for process_instruction. Falling back to no masking")
 
     return code_bytes
-def process_function(func, cur):
+def process_function(func):
     # Perform adress masking
     # Get first instruction and iterate
     code_bytes = []
@@ -117,31 +98,13 @@ def process_function(func, cur):
         code_bytes.extend(process_instruction(instruction))
         instruction = instruction.getNext()
 
-    result = {}
-    result["name"] = func.getName()
-    entrypoint = func.getEntryPoint()
-    result["vaddr"] = "0x" + entrypoint.toString()
-    result["fileoffset"] = str(mem.getAddressSourceInfo(entrypoint).getFileOffset())
-# For size to work, ghidra analysis should be run
-# In case ghidra did not run analysis, the returned size is always 1
-    result["size"] = str(func.getBody().getNumAddresses())
-# Get function bytes out of a stupid java object
-    result["hash"] = ssdeep.hash(bytes(code_bytes))
-    row = (result['size'], cp.getName(), cp.getExecutablePath(), result['name'], result['vaddr'], result['fileoffset'], result['hash'])
-    return row
-
-data = []
-for func in funcs:
-    if(func.isThunk()):
-        continue
-    data.append(process_function(func, cur))
-    if(len(data) > 1000):
-        cur.executemany("INSERT INTO hashdump VALUES(?, ?, ?, ?, ?, ?, ?)", data)
-        data.clear()
+    hash = ssdeep.hash(bytes(code_bytes))
+    return (hash, code_bytes)
 
 
-cur.executemany("INSERT INTO hashdump VALUES(?, ?, ?, ?, ?, ?, ?)", data)
-data.clear()
-hashdb.commit()
-hashdb.close()
-
+funcaddr = askAddress("Function address","Enter Function address")
+fm = cp.getFunctionManager()
+func = fm.getFunctionAt(funcaddr)
+hash,code = process_function(func)
+print(hash)
+print(" ".join(["0x{:02x}".format(x) for x in code]))
