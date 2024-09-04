77848653c8daaca01a233be4f26cc2ce010289e9
R Sai Ashwin <sai.ashwin2409@gmail.com>
2024-09-04 13:41:08 +0530

    Functional masking

--- ghidra_scripts/DumpFunctionInformation_FA.py
+++ ghidra_scripts/DumpFunctionInformation_FA.py
@@ -14,6 +14,10 @@ import sqlite3
 import os
 import ssdeep
 import time
+from ghidra.program.model.listing import Instruction
+from ghidra.program.model.address import Address
+from ghidra.program.flatapi import FlatProgramAPI
+from ghidra.program.model.lang import OperandType
 
 # Set FIRMAL_DIR in env for output file location
 if 'FIRMAL_DIR' not in os.environ:
@@ -33,9 +37,13 @@ if(res.fetchone() is None):
     cur.execute("CREATE TABLE hashdump(size, progname, progpath, funcname, vaddr, fileoff, hash)")
 
 cp = currentProgram()
+ctx = {}
+ctx["ARCH"] = cp.getLanguage().toString().split("/")[0]
+ctx["BITS"] = cp.getLanguage().toString().split("/")[2]
+ctx["DBPATH"] = op_path+"hash.db"
+ctx["TIME"] = timestr
+ctx["FILEPATH"] = cp.getExecutablePath()
 
-# Filename convention : prog_name-arch:endianness:64/32bit:compiler-spec-time.csv
-# Ex: analysis on libc shows: libc.so.6-x86:LE:64:default-20240821:141528.csv
 # Let script be run on headless mode with -noanalyse flag
 # Set minimum number of analysis options
 opts = getCurrentAnalysisOptionsAndValues(cp)
@@ -51,11 +59,69 @@ funcs = cp.getFunctionManager().getFunctions(True)
 addr = cp.getAddressFactory()
 mem = cp.getMemory()
 
-# Gather and write results
-data = []
-for func in funcs:
-    if(func.isThunk()):
-        continue
+def mask_ARM(insn):
+    res = []
+    mask = 0
+    numOp = insn.getNumOperands()
+    for i in range(numOp):
+        mask = mask | int(insn.getPrototype().getOperandValueMask(i).toString(), 16)
+    BITS = int(ctx["BITS"])
+    mask = ( 1 << BITS ) - 1 - mask
+    insn_int = insn.getUnsignedInt(0)
+    insn_int = insn_int & mask
+    while(insn_int > 0):
+        res.append(insn_int & 0xFF)
+        insn_int = insn_int >> 8
+
+    return res
+
+def mask_ARM_THUMB(insn):
+    res = []
+    mask = 0
+    numOp = insn.getNumOperands()
+    for i in range(numOp):
+        mask = mask | int(insn.getPrototype().getOperandValueMask(i).toString(), 16)
+    BITS = int(ctx["BITS"])
+    mask = ( 1 << BITS ) - 1 - mask
+    insn_int = insn.getUnsignedShort(0)
+    insn_int = insn_int & mask
+
+    while(insn_int > 0):
+        res.append(insn_int & 0xFF)
+        insn_int = insn_int >> 8
+
+    return res
+
+def process_instruction(instruction):
+    code_bytes = bytes(map(lambda b: b &0xff, instruction.getBytes()))
+    # Architecture specific code follows.
+    # Register and memory masking is implemented for ARM only
+    match ctx["ARCH"]:
+        case "ARM":
+            # Deconstructing arm is easier than other archs i hope
+
+            # 4 byte instruction:
+            if(len(code_bytes) == 4):
+                code_bytes = mask_ARM(instruction)
+
+            # 2 Byte instruction (thumb)
+            if(len(code_bytes) == 2):
+                code_bytes = mask_ARM_THUMB(instruction)
+        case _:
+            print("Unknown Architecture for process_instruction. Falling back to no masking")
+
+    return code_bytes
+def process_function(func, cur):
+    # Perform adress masking
+    # Get first instruction and iterate
+    code_bytes = []
+    instruction = getFirstInstruction(func)
+    while instruction is not None:
+        if(instruction.getMaxAddress() > func.getBody().getMaxAddress()):
+            break
+        code_bytes.extend(process_instruction(instruction))
+        instruction = instruction.getNext()
+
     result = {}
     result["name"] = func.getName()
     entrypoint = func.getEntryPoint()
@@ -65,41 +131,22 @@ for func in funcs:
 # In case ghidra did not run analysis, the returned size is always 1
     result["size"] = str(func.getBody().getNumAddresses())
 # Get function bytes out of a stupid java object
-    func_code = bytes(map(lambda b: b & 0xff, getBytes(entrypoint, int(result["size"]))))
-    result["hash"] = ssdeep.hash(func_code)
+    result["hash"] = ssdeep.hash(bytes(code_bytes))
     row = (result['size'], cp.getName(), cp.getExecutablePath(), result['name'], result['vaddr'], result['fileoffset'], result['hash'])
-    data.append(row)
+    return row
+
+data = []
+for func in funcs:
+    if(func.isThunk()):
+        continue
+    data.append(process_function(func, cur))
     if(len(data) > 1000):
         cur.executemany("INSERT INTO hashdump VALUES(?, ?, ?, ?, ?, ?, ?)", data)
         data.clear()
+
 
 cur.executemany("INSERT INTO hashdump VALUES(?, ?, ?, ?, ?, ?, ?)", data)
 data.clear()
 hashdb.commit()
 hashdb.close()
-###############################
-#	Does not work	      #
-###############################
-'''
-# Write code to dump all function names,
-# vaddr ( ghidra ) and file offsets
-# for further analysis
 
-# Get all functions
-funcs = currentProgram.functionManager.getFunctions(True)
-
-# Get required handles to convert vaddr to file offsets
-# Particularly useful in case currentProgram is NOT bare-metal
-# firmware, and is instead part of an ELF
-
-# Get addressFactory instance to convert addresses to ghidra's internal Address type
-# as required by getAddressSourceInfo
-addr = currentProgram.addressFactory
-
-
-# get handle to program memory
-mem = currentProgram.getMemory()
-
-# To convert address 0xcafecafe to file offset, we simply do
-# file_offset = mem.getAddressSourceInfo(addr.getAddress("0xcafecafe")).fileOffset
-'''
